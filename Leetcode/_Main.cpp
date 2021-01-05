1f0a01760694632062ebca317b7ca820c4bb5918
DESKTOP-3K7OHIE\JaidenJang <digier@naver.com>
2021-01-06 00:43:01 +0900

    Leetcode Main

---
+++ Leetcode/_Main.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <iostream>
+
+#include <string>
+#include <vector>
+
+#include "7.cpp"
+
+int main()
+{
+	Solution Sol;
+	int result = Sol.reverse(12);
+	std::cout << result;
+}
